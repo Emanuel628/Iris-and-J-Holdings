e1845ec289c2c06139aeefb99e7457d92b9426c9
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-06-16 08:21:00 -0400

    Use single global stylesheet entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/tokens.css';
-import './styles/globals.css';
-import './styles/layout.css';
-import './styles/animations.css';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
