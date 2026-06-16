c2193f2c83ab9803316da66068d93513d04c67ce
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-06-16 08:26:24 -0400

    Wire all public and admin pages into app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import Home from './pages/public/Home';
+import Buy from './pages/public/Buy';
+import Sell from './pages/public/Sell';
+import HomeValue from './pages/public/HomeValue';
+import MobileNotary from './pages/public/MobileNotary';
+import Resources from './pages/public/Resources';
+import About from './pages/public/About';
+import BookContact from './pages/public/BookContact';
+import VacationRentals from './pages/public/VacationRentals';
+import PrivacyPolicy from './pages/public/PrivacyPolicy';
+import TermsOfUse from './pages/public/TermsOfUse';
+import AdminLogin from './pages/admin/AdminLogin';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import Leads from './pages/admin/Leads';
+import Appointments from './pages/admin/Appointments';
+import MediaLibrary from './pages/admin/MediaLibrary';
+import SiteSettings from './pages/admin/SiteSettings';
+
+const routes: Record<string, () => JSX.Element> = {
+  '/': Home,
+  '/buy': Buy,
+  '/sell': Sell,
+  '/home-value': HomeValue,
+  '/mobile-notary': MobileNotary,
+  '/resources': Resources,
+  '/about': About,
+  '/book': BookContact,
+  '/vacation-rentals': VacationRentals,
+  '/privacy': PrivacyPolicy,
+  '/terms': TermsOfUse,
+  '/admin/login': AdminLogin,
+  '/admin': AdminDashboard,
+  '/admin/leads': Leads,
+  '/admin/appointments': Appointments,
+  '/admin/media': MediaLibrary,
+  '/admin/settings': SiteSettings,
+};
 
 function App() {
-  return <Home />;
+  const Page = routes[window.location.pathname] ?? Home;
+  return <Page />;
 }
 
 export default App;
