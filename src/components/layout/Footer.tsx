6ad86fb5c49d7b7a51c1f4425727b6042ac05f3b
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-06-18 07:42:44 -0400

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -34,9 +34,8 @@ function Footer() {
         <div>
           <a href="/privacy">Privacy Policy</a>
           <a href="/terms">Terms of Use</a>
-        <div>
-        <p>© 2026 Iris &amp; J Holdings. All rights reserved.</p>
-      </div>
+          <p>© 2026 Iris &amp; J Holdings. All rights reserved.</p>
+        </div>
     </footer>
   );
 }
